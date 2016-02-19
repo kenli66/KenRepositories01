734404c782cfe36f2b4dcba4de936ceee5cac4c0
ken li <kenli66@hotmail.com>
2016-02-19 16:16:12 +0800

    5th test for git checkout and reset.

--- mystruts/WEB-INF/src/hoge/EchoAction.java
+++ mystruts/WEB-INF/src/hoge/EchoAction.java
@@ -14,6 +14,7 @@ public class EchoAction {
 	//2nd test for git checkout and reset.
 	// 3rd test for git checkout and reset.
 	//4th test for git checkout and reset.
+	//5th test for git checkout and reset.
 	public String execute() {
 		System.out.println("call execute. " + "message=" + message);
 		return "ok";
